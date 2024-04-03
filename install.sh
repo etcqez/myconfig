5ee77208537e462a2f44d7c8db35501121537a87
etcqez <etcqez@outlook.com>
2024-04-03 23:42:38 +0800

    更新配置文件

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@ set -u
 # zsh
 ln -s ~/myconfig/_zprofile ~/.zprofile
 
+# root
+sudo sh -c "cat /home/f/myconfig/zshalias > /root/.bashrc"
+
+#sysctl
+sudo ln -s ~/myconfig/sysctl.conf /etc/sysctl.d/99-sysctl.conf
+
 # keyd
 sudo pacman -S --need --noconfirm keyd
 sudo ln -s /home/f/myconfig/appdate/default.conf /etc/keyd
@@ -10,3 +16,6 @@ sudo ln -s /home/f/myconfig/appdate/default.conf /etc/keyd
 # rime
 mkdir /home/f/.local/share/fcitx5
 ln -s /home/f/myconfig/appdate/rime /home/f/.local/share/fcitx5
+
+# fonts
+ln -sf /home/f/myconfig/appdate/fonts /home/f/.local/share/fonts
