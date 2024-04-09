65eddbcb1041deee9dc319685f2f6d7e924ac395
etcqez <etcqez@outlook.com>
2024-04-09 08:37:00 +0800

    更新配置文件

--- install.sh
+++ install.sh
@@ -6,15 +6,18 @@ ln -sf ~/myconfig/appdate/userprefs.conf ~/.config/hypr/userprefs.conf
 # zsh
 ln -s ~/myconfig/_zprofile ~/.zprofile
 
-# root
+# rootbash
 sudo sh -c "cat /home/f/myconfig/zshalias > /root/.bashrc"
 
-#sysctl
+# sysctl
 sudo ln -s ~/myconfig/sysctl.conf /etc/sysctl.d/99-sysctl.conf
 
 # keyd
 sudo pacman -S --need --noconfirm keyd
 sudo ln -s /home/f/myconfig/appdate/default.conf /etc/keyd
+
+# usb-wakeup
+sudo ln -sf /home/f/myconfig/90-usb-wakeup.rules /etc/udev/rules.d/90-usb-wakeup.rules
 
 # rime
 mkdir /home/f/.local/share/fcitx5
--- soft.sh
+++ soft.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --need pulseaudio cpupower emacs neovim xclip neofetch firefox flameshot screen network-manager-applet dosfstools reflector docker iotop qt5ct smartmontools v2raya-bin v2ray gparted woeusb vlc htop ntfs-3g newsflash os-prober kitty ripgrep gammastep ncompress apache nginx dolphin
+sudo pacman -S --need pulseaudio cpupower emacs neovim xclip neofetch firefox flameshot screen network-manager-applet dosfstools reflector docker iotop qt5ct smartmontools v2raya-bin v2ray gparted woeusb vlc htop ntfs-3g newsflash os-prober kitty ripgrep gammastep ncompress apache nginx dolphin tcpdump
 
 # pip
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
