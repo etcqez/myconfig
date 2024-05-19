70dfb86ebad6cde84685808fe460195d9b928a7b
etcqez <etcqez@outlook.com>
2024-05-19 21:15:31 +0800

    更新配置文件

--- chroot-gentoo.sh
+++ chroot-gentoo.sh
@@ -1,5 +1,4 @@
-sudo mount -o defaults,noatime,compress=zstd,commit=120,subvol=@ /dev/mapper/vg_linux-lv_gentoo /mnt/gentoo
-sudo mkdir -p /mnt/gentoo/efi
 sudo umount /efi
+sudo mount -o defaults,noatime,compress=zstd,commit=120,subvol=@ /dev/mapper/vg_linux-lv_gentoo /mnt/gentoo
 sudo mount /dev/nvme0n1p1 /mnt/gentoo/efi
 sudo arch-chroot /mnt/gentoo
