ce8f53c48cae90bd46ebd5c6026f67209d471ad3
etcqez <etcqez@outlook.com>
2024-05-23 19:07:19 +0800

    update

--- archconfig.sh
+++
@@ -1,4 +0,0 @@
-# reflector
-sudo pacman -S --need --noconfirm reflector
-sudo ln -sf /home/f/myconfig/appdate/reflector.conf /etc/xdg/reflector/reflector.conf
-sudo systemctl enable --now reflector.timer
