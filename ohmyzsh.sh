780b82483fa90ea8d61121e3b634552092f4007a
etcqez <etcqez@outlook.com>
2024-06-05 20:07:24 +0800

    更新配置文件

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -4,5 +4,5 @@ ZSH_CUSTOM=~/.oh-my-zsh/custom
 git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sed -Ei '/^plugins=/s/\)$/ zsh-autosuggestions zsh-syntax-highlighting\)/' ~/.zshrc
-sed -Ei '/^plugins=/s/\)$/ sudo\)/' ~/.zshrc
+sed -Ei '/^plugins=/s/\)$/ sudo z\)/' ~/.zshrc
 echo source ~/myconfig/zshalias >> ~/.zshrc
