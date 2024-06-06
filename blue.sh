89a694c641cc7fd8d32fee8f1225cb9572a0f5dd
etcqez <etcqez@outlook.com>
2024-06-06 21:00:33 +0800

    更新配置文件

--- blue.sh
+++ blue.sh
@@ -1,3 +1,1 @@
-sudo sh -c "echo load-module module-switch-on-connect" >> /etc/pulse/default.pa
-Disable=Headset
-MultiProfile=multiple
+sudo sh -c "echo load-module module-switch-on-connect >> /etc/pulse/default.pa"
