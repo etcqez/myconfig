2651aa1481ed7c2056b7c4e2251d59415e08844a
etcqez <etcqez@outlook.com>
2024-06-07 02:27:45 +0800

    更新配置文件

--- headset.sh
+++ headset.sh
@@ -1,4 +1,3 @@
-sudo test ! -d /var/lib/bluetooth/F0:42:1C:30:00:5B/E0:9D:FA:81:FC:AA && sudo mkdir /var/lib/bluetooth/F0:42:1C:30:00:5B/E0:9D:FA:81:FC:AA
-sudo test ! -d /var/lib/bluetooth/EA:07:21:44:BE:F6 && sudo mkdir /var/lib/bluetooth/EA:07:21:44:BE:F6
-sudo ln -f ~/myconfig/bluetooth/EA:07:21:44:BE:F6/* /var/lib/bluetooth/F0:42:1C:30:00:5B/EA:07:21:44:BE:F6/
-sudo ln -f ~/myconfig/bluetooth/E0:9D:FA:81:FC:AA/* /var/lib/bluetooth/F0:42:1C:30:00:5B/E0:9D:FA:81:FC:AA/
+sudo test ! -d /var/lib/bluetooth/F0:42:1C:30:00:5B && sudo mkdir /var/lib/bluetooth/F0:42:1C:30:00:5B
+sudo cp -f ~/myconfig/bluetooth/EA:07:21:44:BE:F6 /var/lib/bluetooth/F0:42:1C:30:00:5B/
+sudo cp -f ~/myconfig/bluetooth/E0:9D:FA:81:FC:AA /var/lib/bluetooth/F0:42:1C:30:00:5B/
