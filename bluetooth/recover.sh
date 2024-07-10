6dbaab1893463bf4d41b0884cd794f6dddfed6b5
etcqez <etcqez@outlook.com>
2024-07-10 18:17:08 +0800

    更新配置文件

--- bluetooth/recover.sh
+++ bluetooth/recover.sh
@@ -1,3 +1,3 @@
-sudo test ! -d /var/lib/bluetooth/ && sudo mkdir /var/lib/bluetooth/
+sudo test -d /var/lib/bluetooth/ && sudo rm -rf /var/lib/bluetooth/
 sudo cp -rf ~/myconfig/bluetooth/bluetooth/ /var/lib/
 sudo chown -R root:root /var/lib/bluetooth/
