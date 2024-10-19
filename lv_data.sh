fbc46daa41f023e270db15c965782a328ecf0740
etcqez <etcqez@outlook.com>
2024-10-19 23:55:16 +0800

    更新配置文件

--- lv_data.sh
+++ lv_data.sh
@@ -3,7 +3,7 @@ sudo mkdir /data && sudo chown $USER:$USSER /data
 #sudo mount /dev/mapper/vg_linux-lv_data /data
 #sudo btrfs su cr /data/@
 #sudo umount -R /data
-#sudo sh -c "echo -e '/dev/mapper/vg_linux-lv_data /data\tbtrfs\trw,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2,commit=120,subvolid=256,subvol=/@\t0 0' >> /etc/fstab"
-sudo sh -c "echo -e '/dev/nvme0n1p7 /data\tbtrfs\trw,noatime' >> /etc/fstab"
+#sudo sh -c "echo '/dev/mapper/vg_linux-lv_data /data\tbtrfs\trw,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2,commit=120,subvolid=256,subvol=/@\t0 0' >> /etc/fstab"
+sudo sh -c "echo '/dev/nvme0n1p7 /data\tbtrfs\trw,noatime' >> /etc/fstab"
 sudo mount -a
 sudo systemctl daemon-reload
