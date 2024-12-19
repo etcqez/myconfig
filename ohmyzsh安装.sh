8da30a712a268fac23f7bf98362dac22364eaad9
etcqez <etcqez@outlook.com>
2024-12-19 19:26:42 +0800

    M ohmyzsh安装.sh

--- ohmyzsh安装.sh
+++ ohmyzsh安装.sh
@@ -4,7 +4,16 @@ ZSH_CUSTOM=~/.oh-my-zsh/custom
 git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git
+git clone --depth 1 https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH_CUSTOM/plugins/zsh-vi-mode
 sed -Ei '/^plugins=/s/\)$/ zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting\)/' ~/.zshrc
 sed -Ei '/^plugins=/s/\)$/ sudo z\)/' ~/.zshrc
+sed -Ei '/^plugins=/s/\)$/ zsh-vi-mode\)/' ~/.zshrc
 echo source ~/myconfig/zshrc >> ~/.zshrc
-echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+echo "# myconf########################################################################" >> ~/.zshrc
+
+# arch
+# source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+# source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# source /usr/share/nvm/init-nvm.sh
+
